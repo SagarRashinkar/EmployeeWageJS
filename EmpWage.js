3515bb19a843664b82bde8e49ab1ce518273feac
Sagar <sagarrashinkar2@gmail.com>
2021-02-06 10:41:46 +0530

    Added: Regext pattern

--- EmpWage.js
+++ EmpWage.js
@@ -1,4 +1,15 @@
 const util = require('./Utility');
+const empData = require('./EmployeePayRoll')
 
-//Using the filter function
-util.empWageCal();
+// util.empWageCal();
+
+try{
+    empData.id = 1;
+    empData.name = "Sagar";
+    empData.salary = 500;
+    empData.gender = "M";
+    empData._startDate = "06/5/2000";
+    console.log(empData.toString());
+}catch(e){
+    console.error(e);
+}
--- EmployeePayRoll.js
+++ EmployeePayRoll.js
@@ -1,61 +1,63 @@
-const userInput = require('readline-sync');
-
 class EmployeePayRoll{
-    constructor(id, name, salary, gender, date) {
-        this._id = id;
-        this._name = name;
-        this._salary = salary;
-        this._gender = gender;
-        this._date = date;
-    }
-    get id(){
-        return this._id;
-    }
-    get name(){
+
+    get name(){ 
         return this._name;
     }
-    get salary(){
+    set name(name)
+    {
+        let pat = RegExp("^[A-Z][a-z]{2,}$");
+        if(pat.test(name))
+            this._name = name;
+        else throw "Enter a valid name!!!!";
+    }
+
+    get id(){ 
+        return this._id;
+    }
+    set id(id){
+        let idPat = RegExp("[1-9]|1[0-9]$");
+        if(idPat.test(id))
+        this._id = id;
+        else throw "Enter a valid ID number!!!!";
+    }
+
+    get salary(){ 
         return this._salary;
     }
+    set salary(salary){
+        let salaryPat = RegExp("[0-9][0-9]+");
+        if(salaryPat.test(salary))
+        this._salary = salary;
+        else throw "Enter a valid Salary!!!!";
+    }
+
     get gender(){
         return this._gender;
     }
-    get startDate(){
-        return this._date;
+    set gender(gender){
+        let genderPat = RegExp("(M|F|m|f)$");
+        if(genderPat.test(gender))
+        this._gender = gender;
+        else throw "Gender is Invalid!!!!";
     }
-    set id(id){
-        this._id = id;
+
+    get startDate(){ 
+        return this._startDate;
     }
-    set name(name){
-        try{
-            var pat = RegExp("^[A-Z][1][a-zA-Z]{2,}")
-            if(pat.test(name)){
-                this._name = name;
-            }
-            else{
-                throw "Enter a valid name!!!";
-            }
-        }
-        catch(err) {
-            console.error(err);
-        }
+    set startDate(startDate){
+        
+        //let datePat = RegExp("([0-2][0-9]|3[0-1]))[./-]([1-9]|[1][0-2])[./-]([200][0-9]{1}|[201][0-9]{1}|[202][0-1]{1})");
+        let datePat = RegExp("([1-2]{1}[0-9]{1}|3[0-1]{1})\/\.\-(0[1-9]{1}|1[1-2]{1})\/\.\-((?:19|20)[0-9][0-9])");
+
+        if(datePat.test(startDate))
+        this._startDate = startDate;
+        else throw "Date is invalid!!!!";
         
     }
-    set salary(salary){
-        this._salary = salary;
-    }
-    set gender(gender){
-        this._gender = gender;
-    }
-    set startDate(date){
-        this._date = date;
+
+    toString(){
+        return "id="+ this.id+", name="+this.name+", salary="+this.salary+", gender="+this._gender+", startdate="+this.startDate;
     }
 }
-let id = userInput.questionInt("Enter Id: ");
-let name = userInput.question("Enter name: ");
-let salary = userInput.questionInt("Enter salary: ");
-let gender = userInput.question("Enter your gender: ");
-let date = userInput.question("Enter start date: ");
-let empPayRoll = new EmployeePayRoll(id, name, salary, gender, date);
-console.log("Employee PayRoll Data: ");
-console.log(empPayRoll._id+" "+empPayRoll._name+" "+empPayRoll._salary+" "+empPayRoll._gender+" "+empPayRoll._date);
+
+module.exports = new EmployeePayRoll();
